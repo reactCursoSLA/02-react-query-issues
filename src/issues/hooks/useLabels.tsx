c78661efe7eeba27dbb1b477f00407869b07d0eb
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-02 02:34:15 -0500

    :zap: hook para llamar react query y establecer data

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -16,29 +16,29 @@ export const useLabels = () => {
     const labelsQuery = useQuery(
         ['labels'],
         getLabels,
-        // {
-        //     staleTime: 1000 * 60 * 60,
-        //     // initialData: [],
-        //     // placeholderData: [],
-        //     placeholderData: [
-        //         {
-        //             id: 725156255,
-        //             node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
-        //             url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
-        //             name: "good first issue (taken)",
-        //             color: "b60205",
-        //             default: false,
-        //         },
-        //         {
-        //             id: 717031390,
-        //             node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
-        //             url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
-        //             name: "good first issue",
-        //             color: "6ce26a",
-        //             default: true
-        //         }
-        //     ]
-        // }
+        {
+            staleTime: 1000 * 60 * 60, // data hara refresh cada hora
+            // initialData: [], //  data que se almanecara en cache y quiere que sea la inical -> sirve antemano que es la data que se va manejar
+            // placeholderData: [], // data que se mostrara mientras se hace la peticion, para mostrale al usario en que puede trabjar
+            placeholderData: [
+                {
+                    id: 725156255,
+                    node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+                    name: "good first issue (taken)",
+                    color: "b60205",
+                    default: false,
+                },
+                {
+                    id: 717031390,
+                    node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                    name: "good first issue",
+                    color: "6ce26a",
+                    default: true
+                }
+            ]
+        }
     );
 
 
