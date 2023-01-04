d5da7f94aff64d40abeaf6d4f33db6607bf9b7ad
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-03 22:46:58 -0500

    :alien: ajuste llamado api

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,5 +3,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
-    headers: {}
+    headers: {
+        Authorization: 'Bearer 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'
+    }
 });
