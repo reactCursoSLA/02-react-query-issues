9c2f3b420b8e9c93e20f818672427879c87f70cf
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-03 23:53:29 -0500

    :bug: token

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const token = import.meta.env.VITE_GITHUB_KEY
+
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: `Bearer ${token}`
     }
 });
