082935234af00d665fb0884d04d314ec1cc0c395
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-04 21:59:35 -0500

    :zap: :alien: setQueryData con actualizacion despues de la o0tra peticion

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -38,6 +38,9 @@ export const IssueItem: FC<Props> = ({ issue }) => {
         queryClient.setQueryData(
             ['issue', issue.number],
             ()=>issue,
+            {
+                updatedAt: new Date().getTime() + 100000
+            }
         )
     }
 
